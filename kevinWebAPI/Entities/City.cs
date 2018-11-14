97ef28d3442a3fac0d751e41d7ebb9e29d169ca9
Obinna Odirionye <odirionye@gmail.com>
2018-11-14 14:27:48 +0100

    chore(Ef COre): switched to EF core to have persistent data around the database

---
+++ kevinWebAPI/Entities/City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kevinWebAPI.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
+    }
+}
+
---
+++ kevinWebAPI/Entities/PointOfInterest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kevinWebAPI.Entities
+{
+    public class PointOfInterest
+    {
+      
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set;  }
+
+        public int CityId { get; set;  }
+
+    }
+}
